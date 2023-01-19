b0ecdd951b926a4aaa286061247bb70590fa47d9
bezark <johnbezark@gmail.com>
2023-01-19 14:46:26 -0500

    Got Testable

---
+++ First2D Game/Mob.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi()%mob_types.size()]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
